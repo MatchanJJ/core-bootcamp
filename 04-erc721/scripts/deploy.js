ed3e769c10c9f384d3f03fa27fb3edd5b64ee554
Jade Palma <jedzxcx1@gmail.com>
2025-02-09 11:20:05 +0800

    nft minting

--- 04-erc721/scripts/deploy.js
+++ 04-erc721/scripts/deploy.js
@@ -10,12 +10,12 @@ async function main() {
   const [deployer] = await hre.ethers.getSigners();
   const maxSupply = 10000; 
   const erc20TokenAddress = "0x3E0A1b2bD478F7C5711f0f157070B0e3E2116ACb"; 
-  const mintPrice = hre.ethers.parseUnits("0.000000000000000001", 18); // 1 wei
+  const mintPrice = hre.ethers.parseUnits("0.000000000000000001", 18); 
   const MyNFT = await hre.ethers.getContractFactory("MyNFT");
   const contract = await MyNFT.deploy(maxSupply, erc20TokenAddress, mintPrice);
 
   await contract.waitForDeployment();
-  console.log(`MyNFT deployed to: ${contract.address}`);
+  console.log(`MyNFT deployed to: ${contract.target}`);
 
   // Approve the allowance
   const ERC20 = await hre.ethers.getContractAt("IERC20", erc20TokenAddress);
